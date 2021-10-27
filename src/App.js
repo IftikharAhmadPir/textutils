69e2c9290c59aedfb136303544f81258bcff4668
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-10-28 00:10:52 +0200

    State and Event Handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      <Navbar title = "TextUtils" home = "Home" about = "About"/>
+      <Navbar title="TextUtils" home="Home" about="About" />
+      <div className="container my-3">
+        <TextForm heading="Enter Text to Analyze Below"/>
+      </div>
     </>
   );
 }
