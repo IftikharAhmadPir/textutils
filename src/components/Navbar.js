0804113eb62779b9e18b817103dc1238d5da3566
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-11-07 03:28:51 +0100

    Some Modifications to the code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,18 +20,8 @@ export default function Navbar(props) {
                     </ul>
                     <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                        <label className="form-check-label" htmlhtmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
                     </div>
-                    {/* <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off"/>
-                        <label className="btn btn-outline-primary" htmlFor="btnradio1">Light</label>
-
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off"/>
-                        <label className="btn btn-outline-primary" htmlFor="btnradio2">Dark</label>
-
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
-                        <label className="btn btn-outline-primary" htmlFor="btnradio3">Green</label>
-                    </div> */}
                 </div>
             </div>
         </nav>
