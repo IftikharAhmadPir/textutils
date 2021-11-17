cbd969e19ca45b0be2977e83a61588a87e140aa1
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-11-18 00:52:27 +0100

    Router Implemented

--- src/App.js
+++ src/App.js
@@ -1,54 +1,81 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react'
-import Alert from './components/Alert';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null);  
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message,type)=>
-  {
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
+      type: type,
     });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
 
-  const toggleMode=()=>{
-    if(mode==='light')
-    {
-      setMode("dark")
-      document.body.style.backgroundColor = '#042743';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Home (Dark)';
+      document.title = "TextUtils - Home (Dark)";
       // setInterval(() => {
       //   document.title = 'You have a new Message';
       // }, 2000);
       // setInterval(() => {
       //   document.title = 'Check Message';
       // }, 1500);
-    }
-    else
-    {
-      setMode("light")
-      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Home (Light)';
+      document.title = "TextUtils - Home (Light)";
     }
   };
 
   return (
     <>
-      <Navbar title="TextUtils" home="Home" about="About" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
+    <Router>
+      <Navbar
+        title="TextUtils"
+        home="Home"
+        about="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter Text to Analyze Below" mode={mode} showAlert={showAlert}/>
-      </div>
+        <Routes>
+        <Route exact path='/about' element={<About/>} />
+        <Route exact path='/' element={<TextForm
+              heading="Enter Text to Analyze Below"
+              mode={mode}
+              showAlert={showAlert}
+            />} />
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+          {/* <Route path="/">
+            <TextForm
+              heading="Enter Text to Analyze Below"
+              mode={mode}
+              showAlert={showAlert}
+            />
+          </Route> */}
+        </Routes>
+        </div>
+        </Router>
     </>
   );
 }
